7dc32ffec31f44aa9d3a544a6c3e09995dc069f6
Zetcode98 <147458187+Zetcode98@users.noreply.github.com>
2023-10-16 19:17:07 -0700

    Add files via upload

---
+++ Praktikum 3/Untitled-1.py
@@ -0,0 +1,10 @@
+nama = "Muhammad Farhan Abdillah"
+print (nama)
+nim = "0110123284"
+print (nim)
+kelas = "SI04"
+print (kelas)
+telepon = "081919761765"
+print (telepon)
+alamat = "Jl.Kampung Mangga Tugu Selatan,Koja,Jakarta Utara"
+print (alamat)
